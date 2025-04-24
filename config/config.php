b9ebc6d61ba908c691973bc00fa1ff5db21e3cf5
Nguyen-Phong-211 <98467973+Nguyen-Phong-211@users.noreply.github.com>
2025-04-24 09:39:57 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('CLIENT_ID', '14462869816-82g7d4hfndcgv33qvq9f3j4dn5ejpjse.apps.googleusercontent.com');
+define('CLIENT_ID', '??????');
 define('CLIENT_SECRET', 'YOUR_CLIENT_SECRET');
-define('REDIRECT_URI', 'http://localhost/php/projectfinal/index.php?page=login');
+define('REDIRECT_URI', '????');
 ?>
